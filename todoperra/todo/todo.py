c75eab52f2eacada0008d6728e0fb518bdf5d1cc
Jesus Salcido <salcido.jesus.j@gmail.com>
2021-01-11 13:26:21 -0700

    video 16 - actualizando todo

--- todoperra/todo/todo.py
+++ todoperra/todo/todo.py
@@ -39,9 +39,41 @@ def create():
 			return redirect(url_for('todo.index'))
 	return render_template('todo/create.html')
 
+def get_todo(id):
+	db, c = get_db()
+	c.execute(
+		'SELECT t.id, t.description, t.completed, t.created_by, t.created_at, u.username FROM todo t JOIN user u ON t.created_by = u.id WHERE t.id = %s', (id,)
+	)
+	todo = c.fetchone()
+	if todo is None:
+		abort(404, "no existe ese todo: {}".format(id))
+
+	return todo
+
 @bp.route('/<int:id>/update', methods=['GET', 'POST'])
 @login_required
 def update(id):
+	todo = get_todo(id)
+
+	if request.method == 'POST':
+		description = request.form['description']
+		completed = True if request.form.get('completed') == 'on' else False
+
+		error = None
+
+		if not description:
+			error = "La descripción es requerida."
+
+		if error is not None:
+			flash(error)
+		else:
+			db, c = get_db()
+			c.execute(
+				'UPDATE todo SET description = %s, completed = %s WHERE id = %s', (description, completed, id)
+			)
+			db.commit()
+			return redirect(url_for('todo.index'))
+
 	return render_template('todo/update.html', todo=todo)
 
 @bp.route('/<int:id>/delete', methods=['POST'])
