d08b309f2387f26b398c1ae582dcecab495ebd73
Jesus Salcido <salcido.jesus.j@gmail.com>
2021-01-11 10:13:44 -0700

    video 5 - login

--- todoperra/todo/auth.py
+++ todoperra/todo/auth.py
@@ -39,3 +39,29 @@ def register():
 		flash(error)
 
 	return render_template('auth/register.html')
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		db, c = get_db()
+		error = None
+		c.execute(
+			'SELECT * FROM user WHERE username = %s', (username,)
+		)
+		user = c.fetchone()
+
+		if user is None:
+			error = 'pendejo ajskj'
+		elif not check_password_hash(user['password'], password):
+			error = "la contra nuez we"
+
+		if error is None:
+			session.clear()
+			session['user_id'] = user['id']
+			return redirect(url_for('index'))
+
+		flash(error)
+
+	return render_template('auth/login.html')
